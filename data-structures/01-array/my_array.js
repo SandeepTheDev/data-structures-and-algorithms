7246e0beb9e2f0997ed408eb49ef90b18c13bc6e
SandeepTheDev <124437404+SandeepTheDev@users.noreply.github.com>
2023-01-19 13:23:13 +0530

    revisited my_array.js

--- data-structures/01-array/my_array.js
+++ data-structures/01-array/my_array.js
@@ -4,49 +4,42 @@ class MyArray {
     this.length = 0;
   }
 
+  // Time complexity: O(1)
   push(element) {
     this.data[this.length] = element;
     this.length++;
     return this.length;
   }
 
-  pop() {
-    let item = this.data[this.length - 1];
-    delete this.data[this.length - 1];
-    this.length--;
-    return item;
-  }
-
+  // Time complexity: O(1)
   get(index) {
     return this.data[index];
   }
 
-  delete(index) {
-    const element = this.data[index];
-    delete this.data[index];
-    for (let i = index + 1; i < this.length; i++) {
-      this.data[i - 1] = this.data[i];
+  // Time complexity: O(1)
+  pop() {
+    if (this.length > 0) {
+      const element = this.data[this.length - 1];
+      delete this.data[this.length - 1];
+      this.length--;
+      return element;
+    } else {
+      return "Array is empty...";
     }
-    index < this.length - 1 && delete this.data[this.length - 1];
-    this.length--;
-    return element;
   }
 
-  print() {
-    let string = "";
-    for (let key in this.data) {
-      string = string + this.data[key] + ", ";
+  // Time complexity: O(n)
+  delete(index) {
+    if (this.data[index]) {
+      const element = this.data[index];
+      for (let i = index; i < this.length; i++) {
+        this.data[i] = this.data[i + 1];
+      }
+      delete this.data[this.length - 1];
+      this.length--;
+      return element;
+    } else {
+      return "404 Not found 😭";
     }
-    string = string.substring(0, string.length - 2);
-    console.log({ data: this.data, length: this.length });
-    return `[${string}] length: ${this.length}`;
   }
 }
-
-const colors = new MyArray();
-colors.push("green");
-colors.push("red");
-colors.push("black");
-colors.push("white");
-colors.delete(3);
-colors.print();
