19701095742d855374ada9490e950a8daf682ede
SandeepTheDev <124437404+SandeepTheDev@users.noreply.github.com>
2023-02-28 11:44:01 +0530

    added MyArray

--- 01-arrays/my_array.js
+++ 01-arrays/my_array.js
@@ -4,42 +4,76 @@ class MyArray {
     this.length = 0;
   }
 
-  // Time complexity: O(1)
-  push(element) {
-    this.data[this.length] = element;
+  push(value) {
+    this.data[this.length] = value;
     this.length++;
-    return this.length;
+    return this;
   }
 
-  // Time complexity: O(1)
-  get(index) {
-    return this.data[index];
-  }
-
-  // Time complexity: O(1)
   pop() {
+    // is `length` greater than 0?
     if (this.length > 0) {
-      const element = this.data[this.length - 1];
+      // delete the last element
       delete this.data[this.length - 1];
       this.length--;
-      return element;
-    } else {
-      return "Array is empty...";
     }
+
+    return this;
   }
 
-  // Time complexity: O(n)
+  // insert element at the start of an array
+  unshift(value) {
+    // is `length` greater than 0?
+    if (this.length > 0) {
+      // create a new object
+      const newData = {};
+      // insert the new element
+      newData[0] = value;
+
+      // copy the `data` to `newData` with incremented key by 1
+      for (let i = 0; i < this.length; i++) {
+        newData[i + 1] = this.data[i];
+      }
+
+      this.data = { ...newData };
+      this.length++;
+    } else {
+      this.push(value);
+    }
+
+    return this;
+  }
+
+  // remove the first element
+  shift() {
+    // is `length` greater than 0?
+    if (this.length > 0) {
+      // replace all elements with the element next to it
+      for (let i = 0; i < this.length; i++) {
+        this.data[i] = this.data[i + 1];
+      }
+
+      // delete the last element
+      delete this.data[this.length - 1];
+      this.length--;
+    }
+
+    return this;
+  }
+
   delete(index) {
+    // is index exists?
     if (this.data[index]) {
-      const element = this.data[index];
+      // replace all elements from given index with the element next to it
       for (let i = index; i < this.length; i++) {
         this.data[i] = this.data[i + 1];
       }
+
+      // delete the last element
       delete this.data[this.length - 1];
       this.length--;
-      return element;
-    } else {
-      return "404 Not found 😭";
     }
+
+    return this;
   }
 }
